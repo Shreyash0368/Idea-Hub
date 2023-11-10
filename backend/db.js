d9cfb24d5108f230dcf38041f861a7010afff580
Shreyash0368 <shreyashgpt0503@gmail.com>
2023-11-10 17:11:33 +0530

    basic setup of mongodb and express js

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const mongURI = "mongodb://127.0.0.1:27017"
+
+const connectToMongo = async () => {
+    let data = await mongoose.connect(mongURI)
+    console.log("connection established with Mongo");
+}
+
+
+
+// console.log('connected');
+
+module.exports = connectToMongo;
