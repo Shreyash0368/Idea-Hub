3f72d6b8d2a4615c5fd07e80c801f7a4dc850ef6
Shreyash0368 <shreyashgpt0503@gmail.com>
2023-11-11 14:11:09 +0530

    added external routes module

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,16 @@
 const connectToMongo = require('./db')
 const express = require('express')
+const auth = require('./Routes/auth')
+const notes = require('./Routes/notes')
 
 connectToMongo()
 const app = express()
 const port = 3000
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use('/auth', auth);
+app.use('/notes', notes);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
