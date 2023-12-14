07632f1fd361b763fa4ae40971a4118c1e758764
Shreyash0368 <shreyashgpt0503@gmail.com>
2023-12-14 11:08:42 +0530

    added cors and jws

--- backend/db.js
+++ backend/db.js
@@ -2,12 +2,8 @@ const mongoose = require('mongoose');
 const mongURI = "mongodb://127.0.0.1:27017/i-note"
 
 const connectToMongo = async () => {
-    let data = await mongoose.connect(mongURI)
+    await mongoose.connect(mongURI);
     console.log("connection established with Mongo");
 }
-
-
-
-// console.log('connected');
 
 module.exports = connectToMongo;
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const connectToMongo = require('./db')
 const express = require('express')
+var cors = require('cors')
+
 const auth = require('./Routes/auth')
 const notes = require('./Routes/notes')
 
@@ -7,11 +9,12 @@ connectToMongo()
 const app = express()
 const port = 5000
 app.use(express.json())
+app.use(cors())
 
-app.use('/auth', auth);
-app.use('/notes', notes);
+app.use('/api/auth', auth);
+app.use('/api/notes', notes);
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`INote app listening on port ${port}`)
 })
