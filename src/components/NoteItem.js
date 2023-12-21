16e4dd06be21620616beba77b923aa5c6fc8cd80
Shreyash0368 <shreyashgpt0503@gmail.com>
2023-12-21 11:45:30 +0530

    note dit function on client side

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -3,21 +3,21 @@ import Card from 'react-bootstrap/Card';
 import { useNoteContext } from '../NoteContext';
 
 
-export default function NoteItem(props) {
+export default function NoteItem({note, onEditNote}) {
   const {deleteNote, editNote} = useNoteContext();
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Body>
-        <Card.Title>{props.note.title}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">{props.note.tag}</Card.Subtitle>
+        <Card.Title>{note.title}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">{note.tag}</Card.Subtitle>
         <Card.Text>
-          {props.note.description}
+          {note.description}
         </Card.Text>
         <footer className="blockquote-footer">
-          {new Date(props.note.date).toLocaleString()}
+          {new Date(note.date).toLocaleString()}
         </footer>
-        <Card.Link onClick={() => deleteNote(props.note._id)}><i className="fa-solid fa-trash"></i></Card.Link>
-        <Card.Link><i className="fa-regular fa-pen-to-square"></i></Card.Link>
+        <Card.Link onClick={() => deleteNote(note._id)}><i className="fa-solid fa-trash"></i></Card.Link>
+        <Card.Link onClick={() => onEditNote(note._id)}><i className="fa-regular fa-pen-to-square"></i></Card.Link>
       </Card.Body>
     </Card>
   )
--- src/components/NotesDisplay.js
+++ src/components/NotesDisplay.js
@@ -1,29 +1,50 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import { useNoteContext } from '../NoteContext';
 import NoteItem from './NoteItem';
+import EditModal from './EditModal';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 export default function NotesDisplay() {
-    const { notes, getAllNotes } = useNoteContext()  
+    const { notes, getAllNotes, editNote} = useNoteContext()      
+    const [show, setShow] = useState(false)
+    
+    // this id is only used for editing the note (the handleshow passed to each note sets the id which is used by handle save to make api call)
+    const [id, setId] = useState('');
+    
+    const handleShow = (noteid) => {
+        setId(noteid);
+        setShow(true);
+    }
+    const handleClose = () => {setShow(false)}
+
+    const handleSave = (title, tag, description) => {
+        // console.log(title, tag, description);
+        editNote(id, title, tag, description);
+        setShow(false);
+    }
+    
     useEffect(() => {     
         getAllNotes();      
     }, [])
       
     return (
-        <div className="container my-3">
-            <Container>
-                <Row>
-                    {notes.map((note) => {
-                        return (
-                            <Col key={note._id} className="mb-3">
-                                <NoteItem note={note} />
-                            </Col>
-                        )
-                    })}
-                </Row>
-            </Container>            
-        </div>
+        <>
+            <EditModal show={show} handleClose={handleClose} handleSave={handleSave}/>
+            <div className="container my-3">
+                <Container>
+                    <Row>
+                        {notes.map((note) => {
+                            return (
+                                <Col key={note._id} className="mb-3">
+                                    <NoteItem note={note} onEditNote={handleShow}/>
+                                </Col>
+                            )
+                        })}
+                    </Row>
+                </Container>            
+            </div>
+        </>
     )
 }
