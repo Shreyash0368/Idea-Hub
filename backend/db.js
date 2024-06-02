8a274757c3f120a60059e7a95ac7622d5b9deb84
Shreyash0368 <shreyashgpt0503@gmail.com>
2024-06-02 17:14:37 +0530

    removed env log

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 const mongURI = process.env.MONGO_URI;
-console.log("-------------");
-console.log(mongURI);
-console.log("-------------");
 
 const connectToMongo = async () => {
     await mongoose.connect(mongURI);
